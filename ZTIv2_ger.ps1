1b6ee50568c7e7973cfae842b0352d908b4b7dbb
nandorforai <nforai@fiduxit.com>
2026-01-14 13:29:00 +0100

    Create ZTIv2_ger.ps1

---
+++ ZTIv2_ger.ps1
@@ -0,0 +1,12 @@
+# OSDCloud ZTI skripta za cloud
+$Global:MyOSDCloud = [ordered]@{
+    Restart              = $true
+    RecoveryPartition    = $true
+    WindowsUpdate        = $true
+    WindowsUpdateDrivers = $true
+    ClearDiskConfirm     = $false
+    ClearDisk            = $true
+    DiskNumber           = 0
+}
+
+Start-OSDCloud -OSName "Windows 11 25H2 x64" -OSEdition Enterprise -OSActivation Volume -OSLanguage de-DE -ZTI
